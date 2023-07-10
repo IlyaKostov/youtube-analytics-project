5cf8874b08143d18b70470bcd716e9f79c6a9e8a
Kostov Ilya <kick1991@mail.ru>
2023-07-10 20:50:04 +0500

    homework 6

--- src/video.py
+++ src/video.py
@@ -1,18 +1,30 @@
 import os
 from googleapiclient.discovery import build
 
+from src.wrong_id import WrongId
+
 YT_API_KEY = os.getenv('YT_API_KEY')
 
 
 class Video:
     def __init__(self, video_id):
         self.__video_id = video_id
-        self.video = self.get_service().videos().list(id=self.__video_id,
-                                                      part='snippet,contentDetails,statistics').execute()
-        self.title = self.video['items'][0]['snippet']['title']
-        self.url = f'https://youtu.be/{self.__video_id}'
-        self.view_count = self.video['items'][0]['statistics']['viewCount']
-        self.like_count = self.video['items'][0]['statistics']['likeCount']
+        try:
+            self.video = self.get_service().videos().list(id=self.__video_id,
+                                                          part='snippet,contentDetails,statistics').execute()
+            if len(self.video['items']) == 0:
+                raise WrongId
+        except WrongId as a:
+            print(a.message)
+            self.title = None
+            self.url = None
+            self.view_count = None
+            self.like_count = None
+        else:
+            self.title = self.video['items'][0]['snippet']['title']
+            self.url = f'https://youtu.be/{self.__video_id}'
+            self.view_count = self.video['items'][0]['statistics']['viewCount']
+            self.like_count = self.video['items'][0]['statistics']['likeCount']
 
     @classmethod
     def get_service(cls):
